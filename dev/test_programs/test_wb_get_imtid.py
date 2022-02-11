f2c060eeb3bdfe8f4dd080103cd1817ce2b29504
Roman Rozhalovskiy <roman.for.wb@gmail.com>
2022-02-11 12:34:55 +0300

    removing cached files

--- dev/test_programs/test_wb_get_imtid.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-response = requests.get('https://wbx-content-v2.wbstatic.net/ru/17094599.json')
-result = response.json()
-for key, value in result.items():
-    print(f"{key} : {value}")
--- test.py
+++
@@ -1,9 +0,0 @@
-import modules.google_work as google_work
-import modules.files as files
-import modules.wildberries as wildberries
-
-import requests
-
-worksheet = google_work.open_sheet(files.get_google_key('wb_analytics'), 'Заказы (тест)')
-orders_table = wildberries.orders('ahmetov')
-google_work.insert_table(worksheet, orders_table, replace=True)

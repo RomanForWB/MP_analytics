df8483a9ab6a36cc9df4d0e8c0218e884cfaba81
Roman Rozhalovskiy <roman.for.wb@gmail.com>
2022-02-15 13:23:00 +0300

    async representation

--- modules/async_requests.py
+++ modules/async_requests.py
@@ -3,9 +3,10 @@ import aiohttp, asyncio
 counter = 0
 
 
-async def _get_fetch(session, url, id, params=None, headers=None, content_type='text'):
+async def _get_fetch(session, url, id,
+                     params=None, headers=None, content_type='text'):
     global counter
-    while (True):
+    while True:
         try:
             async with session.get(url, params=params, headers=headers) as response:
                 if response.status < 200 or response.status >= 300:
@@ -23,8 +24,9 @@ async def _get_fetch(session, url, id, params=None, headers=None, content_type='
         except asyncio.exceptions.TimeoutError: pass
 
 
-async def _post_fetch(session, url, id, body, params=None, headers=None, content_type='text'):
-    while (True):
+async def _post_fetch(session, url, id, body,
+                      params=None, headers=None, content_type='text'):
+    while True:
         try:
             async with session.post(url, params=params, json=body, headers=headers) as response:
                 if response.status < 200 or response.status >= 300: pass
@@ -42,7 +44,8 @@ async def _post_fetch(session, url, id, body, params=None, headers=None, content
         except asyncio.exceptions.TimeoutError: pass
 
 
-async def _fetch_all_tasks_by_urls(http_method, session, url_list, ids, params=None, body=None, headers=None, content_type='text'):
+async def _fetch_all_tasks_by_urls(http_method, session, url_list, ids,
+                                   params=None, body=None, headers=None, content_type='text'):
     tasks = list()
     if len(url_list) != len(ids): raise IndexError("Length of urls list must be equal to ids list.")
 
@@ -65,7 +68,8 @@ async def _fetch_all_tasks_by_urls(http_method, session, url_list, ids, params=N
     return results
 
 
-async def _fetch_all_tasks_by_params(http_method, session, url, params_list, ids, body=None, headers=None, content_type='text'):
+async def _fetch_all_tasks_by_params(http_method, session, url, params_list, ids,
+                                     body=None, headers=None, content_type='text'):
     tasks = list()
     if len(params_list) != len(ids): raise IndexError("Length of params list must be equal to ids list.")
     if http_method.lower() == 'get':
@@ -87,7 +91,8 @@ async def _fetch_all_tasks_by_params(http_method, session, url, params_list, ids
     return results
 
 
-async def _fetch_all_tasks_by_headers(http_method, session, url, headers_list, ids, params=None, body=None, content_type='text'):
+async def _fetch_all_tasks_by_headers(http_method, session, url, headers_list, ids,
+                                      params=None, body=None, content_type='text'):
     tasks = list()
     if len(headers_list) != len(ids): raise IndexError("Length of headers list must be equal to ids list.")
     if http_method.lower() == 'get':
@@ -109,7 +114,8 @@ async def _fetch_all_tasks_by_headers(http_method, session, url, headers_list, i
     return results
 
 
-async def _fetch_all_tasks_by_bodies(http_method, session, url, bodies_list, ids, params=None, headers=None, content_type='text'):
+async def _fetch_all_tasks_by_bodies(http_method, session, url, bodies_list, ids,
+                                     params=None, headers=None, content_type='text'):
     tasks = list()
     if len(bodies_list) != len(ids): raise IndexError("Length of headers list must be equal to ids list.")
     if http_method.lower() == 'get':
@@ -131,7 +137,8 @@ async def _fetch_all_tasks_by_bodies(http_method, session, url, bodies_list, ids
     return results
 
 
-async def _fetch_all_by_urls(http_method, url_list, ids, params=None, body=None, headers=None, content_type='text'):
+async def _fetch_all_by_urls(http_method, url_list, ids,
+                             params=None, body=None, headers=None, content_type='text'):
     global counter
     counter = 0
     timeout = aiohttp.ClientTimeout(total=10)
@@ -148,7 +155,8 @@ async def _fetch_all_by_urls(http_method, url_list, ids, params=None, body=None,
         return result_dict
 
 
-async def _fetch_all_by_params(http_method, url, params_list, ids, body=None, headers=None, content_type='text'):
+async def _fetch_all_by_params(http_method, url, params_list, ids,
+                               body=None, headers=None, content_type='text'):
     global counter
     counter = 0
     async with aiohttp.ClientSession() as session:
@@ -163,7 +171,8 @@ async def _fetch_all_by_params(http_method, url, params_list, ids, body=None, he
         return result_dict
 
 
-async def _fetch_all_by_headers(http_method, url, headers_list, ids, params=None, body=None, content_type='text'):
+async def _fetch_all_by_headers(http_method, url, headers_list, ids,
+                                params=None, body=None, content_type='text'):
     global counter
     counter = 0
     async with aiohttp.ClientSession() as session:
@@ -178,64 +187,69 @@ async def _fetch_all_by_headers(http_method, url, headers_list, ids, params=None
         return result_dict
 
 
-async def _fetch_all_by_bodies(http_method, url, bodies_list, ids, params=None, headers=None, content_type='text'):
+async def _fetch_all_by_bodies(http_method, url, bodies_list, ids,
+                               params=None, headers=None, content_type='text'):
     global counter
     counter = 0
     async with aiohttp.ClientSession() as session:
         print(f"Подключение к {url.split('//')[1].split('/')[0]}...")
         print(f"Всего запросов: {len(bodies_list)}")
         result_list = await _fetch_all_tasks_by_bodies(http_method, session, url, bodies_list, ids,
-                                                        params=params,
-                                                        headers=headers,
-                                                        content_type=content_type)
+                                                       params=params,
+                                                       headers=headers,
+                                                       content_type=content_type)
         result_dict = {item[0]: item[1] for item in result_list}
         print()
         return result_dict
 
 
-
-def by_urls(http_method, url_list, ids, params=None, body=None, headers=None, content_type='text'):
+def by_urls(http_method, url_list, ids,
+            params=None, body=None, headers=None, content_type='text'):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    while (True):
+    while True:
         try: return asyncio.run(_fetch_all_by_urls(http_method, url_list, ids,
-                                                  params=params,
-                                                  body=body,
-                                                  headers=headers,
-                                                  content_type=content_type))
+                                                   body=body,
+                                                   params=params,
+                                                   headers=headers,
+                                                   content_type=content_type))
         except aiohttp.ClientConnectorError:
             print(f"Не удалось подключиться к {url_list[0].split('//')[1].split('/')[0]}...")
         except aiohttp.ClientPayloadError:
             print(f"Не удалось подключиться к {url_list[0].split('//')[1].split('/')[0]}...")
 
 
-def by_params(http_method, url, params_list, ids, body=None, headers=None, content_type='text'):
+def by_params(http_method, url, params_list, ids,
+              body=None, headers=None, content_type='text'):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    while (True):
+    while True:
         try: return asyncio.run(_fetch_all_by_params(http_method, url, params_list, ids,
-                                                    body=body,
-                                                    headers=headers,
-                                                    content_type=content_type))
-        except aiohttp.ClientConnectorError:
-            print(f"Не удалось подключиться к {url.split('//')[1].split('/')[0]}...")
-        except aiohttp.ClientPayloadError:
-            print(f"Не удалось подключиться к {url.split('//')[1].split('/')[0]}...")
-
-
-def by_headers(http_method, url, headers_list, ids, params=None, body=None, content_type='text'):
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    while (True):
-        try: return asyncio.run(_fetch_all_by_headers(http_method, url, headers_list, ids,
-                                                     params=params,
                                                      body=body,
+                                                     headers=headers,
                                                      content_type=content_type))
         except aiohttp.ClientConnectorError:
             print(f"Не удалось подключиться к {url.split('//')[1].split('/')[0]}...")
         except aiohttp.ClientPayloadError:
             print(f"Не удалось подключиться к {url.split('//')[1].split('/')[0]}...")
 
-def by_bodies(http_method, url, bodies_list, ids, params=None, headers=None, content_type='text'):
+
+def by_headers(http_method, url, headers_list, ids,
+               params=None, body=None, content_type='text'):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    while (True):
+    while True:
+        try: return asyncio.run(_fetch_all_by_headers(http_method, url, headers_list, ids,
+                                                      params=params,
+                                                      body=body,
+                                                      content_type=content_type))
+        except aiohttp.ClientConnectorError:
+            print(f"Не удалось подключиться к {url.split('//')[1].split('/')[0]}...")
+        except aiohttp.ClientPayloadError:
+            print(f"Не удалось подключиться к {url.split('//')[1].split('/')[0]}...")
+
+
+def by_bodies(http_method, url, bodies_list, ids,
+              params=None, headers=None, content_type='text'):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    while True:
         try: return asyncio.run(_fetch_all_by_bodies(http_method, url, bodies_list, ids,
                                                      params=params,
                                                      headers=headers,

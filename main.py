7b49715a6882375606baff130e8c911671385a92
Roman Rozhalovskiy <roman.for.wb@gmail.com>
2022-03-09 15:09:38 +0300

    fixes

--- main.py
+++ main.py
@@ -431,7 +431,8 @@ if __name__ == '__main__':
             google_work.insert_table(worksheet, categories_table, replace=True)
             worksheet = google_work.open_sheet(info.google_key('ozon_analytics'), 'Остатки')
             input_data = ozon_info.all_suppliers()
-            stocks_table = ozon_analytics.stocks(input_data)
+            old_stocks_columns = google_work.get_columns(worksheet, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+            stocks_table = ozon_analytics.stocks(input_data, old_stocks_columns)
             google_work.insert_table(worksheet, stocks_table, replace=True)
             choice = 'start'
         elif choice == 'wb_categories':
